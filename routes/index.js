4423d349f3ecaa83b20df7351b6164d1325acbe4
lisazhou39 <lisazhou39@gmail.com>
2020-02-20 10:18:51 -0800

    viewAlt

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var projects = require('../projects.json');
  */
 
 exports.view = function(request, response){
+	projects["viewAlt"] = false;
+	console.log(projects['viewAlt']);
+  	response.render('index', projects);
+};
+
+exports.viewAlt = function(request, response){
+	projects["viewAlt"] = true;
+	console.log(projects['viewAlt']);
   	response.render('index', projects);
 };
